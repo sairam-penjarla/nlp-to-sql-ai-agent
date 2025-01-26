49c64afeb017f3e2f3578ee257ea951878168851
Sai Ram pinjarla <sairampinjarla@Sais-MacBook-Air.local>
2025-01-26 13:56:55 +0530

    fixed a few bugs in UI

--- run.py
+++ run.py
@@ -1,57 +1,56 @@
-from src.utils import Utilities
-from custom_logger import logger
-import os
-from flask import jsonify
-import traceback
 import json
-from flask import (
-    Flask,
-    Response,
-    jsonify, 
-    render_template, 
-    request
-)
-from src.multi_shot_examples import get_exmaples
-from src.prompt_templates import ( 
-    COMPLETE_SCHEMA,
-    COLUMN_GUIDELINES,
-    QUERY_GUIDELINES,
-    AGENT_GUIDELINES
-)
+import os
 import traceback
+
+from flask import Flask, Response, jsonify, render_template, request
+
+from custom_logger import logger
+from src.multi_shot_examples import get_exmaples
+from src.prompt_templates import (
+    AGENT_GUIDELINES,
+    COLUMN_GUIDELINES,
+    COMPLETE_SCHEMA,
+    QUERY_GUIDELINES,
+)
+from src.utils import Utilities
 
 app = Flask(__name__)
 utils = Utilities()
 
-queries_conversation = get_exmaples('queries_conversation')
-agent_conversation = get_exmaples('agent_conversation')
+queries_conversation = get_exmaples("queries_conversation")
+agent_conversation = get_exmaples("agent_conversation")
+
 
 @app.route("/")
 def landing_page():
     try:
         previous_session_meta_data = utils.session_utils.get_session_meta_data()
-        return render_template("index.html", previous_session_meta_data=previous_session_meta_data)
+        return render_template(
+            "index.html", previous_session_meta_data=previous_session_meta_data
+        )
     except Exception:
         print("Error in landing_page function:")
         print(traceback.format_exc())
         return jsonify({"error": "An internal server error occurred"}), 500
 
-@app.route('/get_random_session_icon', methods=['POST'])
+
+@app.route("/get_random_session_icon", methods=["POST"])
 def get_random_session_icon():
     try:
         data = request.get_json()
-        session_id = data.get('session_id')
+        session_id = data.get("session_id")
         return jsonify({"relavant_schema": utils.get_session_icon(session_id)})
     except Exception:
         print("Error in landing_page function:")
         print(traceback.format_exc())
         return jsonify({"error": "An internal server error occurred"}), 500
 
-@app.route('/extract_relavant_schema', methods=['POST'])
+
+@app.route("/extract_relavant_schema", methods=["POST"])
 def extract_relavant_schema():
     try:
         data = request.get_json()
-        user_input = data.get('user_input')
+        user_input = data.get("user_input")
 
         msg = utils.get_user_msg(
             content=COLUMN_GUIDELINES + COMPLETE_SCHEMA,
@@ -67,13 +66,13 @@ def extract_relavant_schema():
         return jsonify({"error": "An internal server error occurred"}), 500
 
 
-@app.route('/get_session_data', methods=['POST'])
+@app.route("/get_session_data", methods=["POST"])
 def get_session_data():
     try:
         data = request.get_json()
-        session_id = data.get('sessionId')
+        session_id = data.get("sessionId")
         session_data = utils.session_utils.get_session_data(session_id)
-        return jsonify({'session_data': session_data})
+        return jsonify({"session_data": session_data})
 
     except Exception:
         print("Error in get_session_data function:")
@@ -81,11 +80,11 @@ def get_session_data():
         return jsonify({"error": "An internal server error occurred"}), 500
 
 
-@app.route('/delete_session', methods=['POST'])
+@app.route("/delete_session", methods=["POST"])
 def delete_session():
     try:
         data = request.get_json()
-        session_id = data.get('session_id')
+        session_id = data.get("session_id")
 
         if not session_id:
             return jsonify({"error": "Session ID is required."}), 400
@@ -99,7 +98,7 @@ def delete_session():
         return jsonify({"error": "An internal server error occurred"}), 500
 
 
-@app.route('/delete_all_sessions', methods=['POST'])
+@app.route("/delete_all_sessions", methods=["POST"])
 def delete_all_sessions():
     try:
         response, status_code = utils.session_utils.delete_all_sessions()
@@ -111,18 +110,20 @@ def delete_all_sessions():
         return jsonify({"error": "An internal server error occurred"}), 500
 
 
-@app.route('/update_session', methods=['POST'])
+@app.route("/update_session", methods=["POST"])
 def update_session():
     try:
         data = request.get_json()
 
-        session_id = data.get('session_id')
-        prompt = data.get('prompt')
-        sql_query = data.get('sql_query')
-        session_icon = data.get('session_icon')
-        chatbot_assistant = data.get('chatbot_assistant')
+        session_id = data.get("session_id")
+        prompt = data.get("prompt")
+        sql_query = data.get("sql_query")
+        session_icon = data.get("session_icon")
+        chatbot_assistant = data.get("chatbot_assistant")
 
-        utils.session_utils.add_data(session_id, prompt, sql_query, chatbot_assistant, session_icon)
+        utils.session_utils.add_data(
+            session_id, prompt, sql_query, chatbot_assistant, session_icon
+        )
 
         return jsonify({"success": True})
 
@@ -132,19 +133,21 @@ def update_session():
         return jsonify({"error": "An internal server error occurred"}), 500
 
 
-@app.route('/generate_sql_query', methods=['POST'])
+@app.route("/generate_sql_query", methods=["POST"])
 def generate_sql_query():
     try:
         data = request.get_json()
-        relavant_schema = data.get('relavant_schema')
-        user_input = data.get('user_input')
-        session_id = data.get('session_id')
+        relavant_schema = data.get("relavant_schema")
+        user_input = data.get("user_input")
+        session_id = data.get("session_id")
 
         # Retrieve previous messages for SQL query generation
-        messages = utils.get_previous_messages(session_id, QUERY_GUIDELINES, "sql_query")
+        messages = utils.get_previous_messages(
+            session_id, QUERY_GUIDELINES, "sql_query"
+        )
         msg = {
             "role": "user",
-            "content": f"\n\nRelevant Schema:{relavant_schema}\n\nQuestion:{user_input}\n\nAnswer:"
+            "content": f"\n\nRelevant Schema:{relavant_schema}\n\nQuestion:{user_input}\n\nAnswer:",
         }
 
         # Generate SQL query
@@ -158,11 +161,11 @@ def generate_sql_query():
         return jsonify({"error": "An internal server error occurred"}), 500
 
 
-@app.route('/execute_sql_query', methods=['POST'])
+@app.route("/execute_sql_query", methods=["POST"])
 def execute_sql_query():
     try:
         data = request.get_json()
-        sql_query = data.get('sql_query')
+        sql_query = data.get("sql_query")
 
         # Execute the SQL query
         sql_data = utils.execute_query(sql_query)
@@ -174,30 +177,32 @@ def execute_sql_query():
         return jsonify({"error": "An internal server error occurred"}), 500
 
 
-@app.route('/invoke_agent', methods=['POST'])
+@app.route("/invoke_agent", methods=["POST"])
 def invoke_agent():
     try:
         data = request.get_json()
-        sql_data = data.get('sql_data')
-        user_input = data.get('user_input')
-        session_id = data.get('session_id')
+        sql_data = data.get("sql_data")
+        user_input = data.get("user_input")
+        session_id = data.get("session_id")
 
         # Retrieve previous messages for chatbot assistant
-        messages = utils.get_previous_messages(session_id, AGENT_GUIDELINES, "chatbot_assistant")
+        messages = utils.get_previous_messages(
+            session_id, AGENT_GUIDELINES, "chatbot_assistant"
+        )
         msg = {
             "role": "user",
-            "content": f"SQL Data:{sql_data}\n\nQuestion:{user_input}\n\nAnswer:"
+            "content": f"SQL Data:{sql_data}\n\nQuestion:{user_input}\n\nAnswer:",
         }
 
         # Stream the response from the LLM
         agent_output = utils.invoke_llm_stream(messages=messages + [msg])
-        return Response(agent_output, content_type='text/event-stream')
+        return Response(agent_output, content_type="text/event-stream")
 
     except Exception:
         print("Error in invoke_agent function:")
         print(traceback.format_exc())
         return jsonify({"error": "An internal server error occurred"}), 500
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
